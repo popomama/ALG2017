227e194f1026243803dd354bf627e5d583f8cdd7
Wei Xia <wei_xia@hotmail.com>
2017-03-22 14:42:12 -0700

    first checkin

---
+++ ALGRKC/ALGRKC/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGRKC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first checkin
+        }
+    }
+}
