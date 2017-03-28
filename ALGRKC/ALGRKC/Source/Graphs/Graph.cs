64fc6bbf04dfacd0e3494b83184d5c611d873fce
Wei Xia <wei_xia@hotmail.com>
2017-03-27 20:15:49 -0700

    implemented Graph class

--- ALGRKC/ALGRKC/Source/Graphs/Graph.cs
+++ ALGRKC/ALGRKC/Source/Graphs/Graph.cs
@@ -5,28 +5,72 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+using ALGRKC.Source.Basic;
+
 namespace ALGRKC.Source.Graphs
 {
-    class Graph
+    public class Graph
     {
-        Graph(int V)
+        int vertNum;//number of vertices
+        int edgeNum;//number of edges
+        Bag<int>[] adj;  //adjacency lists
+
+        public Graph(int v)
         {
+            this.Init(v);
 
         }
 
-        Graph()
+        void Init(int v)
         {
+
+            vertNum = v;
+            adj = new Bag<int>[vertNum];
+            for (int i = 0; i < vertNum; i++)
+                adj[i] = new Bag<int>();
+        }
+
+        public Graph(StreamReader sr)
+        {
+
+            string line = null;
+            line = sr.ReadLine(); // read V;
+            int vNum = Int32.Parse(line);
+            Init(vNum);
+
+            line = sr.ReadLine();// read E
+            this.edgeNum = Int32.Parse(line);
+
+            while((line=sr.ReadLine())!=null)   //each line is an edge
+            {
+                string[] pair = line.Split();
+                int v = Int32.Parse(pair[0]);
+                int w = Int32.Parse(pair[1]);
+                this.AddEdge(v, w); //add edge
+
+            }
+
+
 
         }
 
-        int V()
-        { return 0; }
+        public int V()
+        { return vertNum; }
 
-        int E()
-        { return 0; }
+        public int E()
+        { return edgeNum; }
 
-        void addEdge(int v, int w)
-        { }
+        void AddEdge(int v, int w)
+        {
+            adj[v].Add(w);//add the edge to both vertices' adjacency lists
+            adj[w].Add(v);
+            edgeNum++; // increase the edgeNumber
+        }
+
+        public IEnumerable<int> AdjList(int v)
+        {
+            return adj[v];
+        }
 
        
     }
---
+++ ALGRKC/ALGRKC/Source/Graphs/Search.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGRKC.Source.Graphs
+{
+    class Search
+    {
+        Search(Graph g, int s)
+        {
+
+        }
+
+        bool marked(int v)
+        {
+            return false;
+        }
+
+        int count()
+        {
+            return 0;
+        }
+
+    }
+}
