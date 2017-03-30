781b6ebdf7a7b13b755455b4bb05b1eb6b79603c
Wei Xia <wei_xia@hotmail.com>
2017-03-30 11:47:23 -0700

    added BFS

---
+++ ALGRKC/ALGRKC/Source/Graphs/BreadthFirstPaths.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGRKC.Source.Graphs
+{
+    public class BreadthFirstPaths
+    {
+        int source;
+        int[] edgeTo;
+        bool[] isMarked;
+        public BreadthFirstPaths(Graph g, int s)
+        {
+            source = s;
+            edgeTo = new int[g.V()];
+            isMarked = new bool[g.V()];
+
+        }
+
+        void bfs(Graph g, int s)
+        {
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(s);
+
+            int current;
+
+            while(q.Count!=0)
+            {
+                current = q.Dequeue();
+                isMarked[current] = true;
+                foreach(int v in g.AdjList(current))
+                {
+                    if(!isMarked[v])
+                    {
+                        q.Enqueue(v);
+                        edgeTo[v] = current;
+                    }
+                }
+            }
+        }
+
+        public bool HasPathTo(int destination)
+        {
+            return isMarked[destination];
+        }
+
+        public IEnumerable<int> PathTo(int v)
+        {
+            if (!HasPathTo(v))
+                return null;
+
+            Stack<int> path = new Stack<int>();
+
+            while(v!=source)
+            {
+                path.Push(v);
+                v = edgeTo[v];
+            }
+
+            path.Push(source);
+            return path;
+        }
+}
