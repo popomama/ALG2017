6763cf3fd3d270b0b5650a85b340e7dd5305c607
Wei Xia <wei_xia@hotmail.com>
2017-04-07 20:34:40 -0700

    Added Digraph

---
+++ ALGRKC/ALGRKC/Source/DirectedGraph/Digraph.cs
@@ -0,0 +1,89 @@
+﻿using ALGRKC.Source.Basic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGRKC.Source.DirectedGraph
+{
+    public class Digraph
+    {
+        int vertNum;//number of vertices
+        int edgeNum;//number of edges
+        Bag<int>[] adj;  //adjacency lists
+
+        public Digraph(int v)
+        {
+            Init(v);
+        }
+
+        public Digraph(StreamReader sr)
+        {
+            string line = null; 
+            line = sr.ReadLine();
+            Init(Int32.Parse(line));
+
+            line = sr.ReadLine();
+            int edge = Int32.Parse(line);
+            string[] pair;
+            int source, destination;
+            for(int i=0;i<edge;i++)
+            {
+                line = sr.ReadLine();
+                pair = line.Split(' ');
+                source = Int32.Parse(pair[0]);
+                destination = Int32.Parse(pair[1]);
+
+                AddEdge(source, destination);
+
+
+            }
+
+        }
+
+        void Init(int v)
+        {
+            this.vertNum = v;
+            adj = new Bag<int>[v];
+            for (int i=0;i<v;i++)
+            {
+                adj[i] = new Bag<int>();
+            }
+        }
+        public int V()
+        {
+            return vertNum;
+        }
+
+        public int E()
+        {
+            return edgeNum;
+        }
+
+        public void AddEdge(int source, int destination)
+        {
+            adj[source].Add(destination);//since this is DAG, only one way is needed.
+            edgeNum++;//increase the edge number
+
+        }
+
+        Digraph Reverse()
+        {
+            Digraph g = new Digraph(this.vertNum);
+
+            for(int i=0;i<vertNum;i++)
+            {
+                foreach (int v in adj[i])
+                    g.AddEdge(v, i);
+
+            }
+
+            return g;
+        }
+
+
+    }
+
+}
---
+++ ALGRKC/ALGRKC/Source/Graphs/TwoColor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGRKC.Source.Graphs
+{
+    //Is the graph bipartie? Can the verties of a given graph be assigned one of the two colors in such
+    //a way that no edge connects vertices of the same color's?
+    public class TwoColor
+    {
+        bool[] isMarked;
+        bool[] color;
+        bool isBipartie = true;
+
+
+        public TwoColor(Graph g)
+        {
+            isMarked = new bool[g.V()];
+            color = new bool[g.V()];
+            for (int i = 0; i < g.V(); i++)
+            {
+                if(!isMarked[i])
+                    DFS(g, i, true);
+
+            }
+        }
+
+        void DFS(Graph g, int s, bool currentColor)
+        {
+            isMarked[s] = true;
+            color[s] = currentColor;
+            foreach (int v in g.AdjList(s))
+            {
+                if (!isMarked[v])
+                    DFS(g, v, !currentColor);
+                else
+                {
+                    if (color[s] == color[v])
+                        isBipartie = false;
+                }
+            }
+
+
+        }
+
+        public bool IsBipartie()
+        {
+            return isBipartie;
+        }
+
+    }
+}
