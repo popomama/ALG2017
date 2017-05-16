37e24dd0d4a1873e37a711c9aa874ed14c9cf8b8
Wei Xia <wei_xia@hotmail.com>
2017-05-16 18:39:13 -0400

    added IndexMinPQ

---
+++ ALGRKC/ALGRKC/Source/Basic/IndexMinPQ.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGRKC.Source.Basic
+{
+    //this class implements Index MinPQ
+    class IndexMinPQ <T> where T : IComparable<T>
+    {
+        int maxSize;
+        int size;
+        T[] pq;
+
+        public IndexMinPQ(int maxSize)
+        {
+            this.maxSize = maxSize;
+            pq = new T[maxSize + 1];
+            size = 0;
+
+        }
+    }
+}
