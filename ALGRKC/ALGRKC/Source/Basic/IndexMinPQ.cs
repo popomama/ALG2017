0c59efac0651f4c7fecc22340177b18e4c93c060
Wei Xia <wei_xia@hotmail.com>
2017-05-16 21:06:42 -0700

    updated the IndexMinPQ class

--- ALGRKC/ALGRKC/Source/Basic/IndexMinPQ.cs
+++ ALGRKC/ALGRKC/Source/Basic/IndexMinPQ.cs
@@ -11,14 +11,135 @@ namespace ALGRKC.Source.Basic
     {
         int maxSize;
         int size;
-        T[] pq;
+        T[] keys;//the array stores the keys based on the original index
+        int[] pq; //pq[i] = index of the key in the qp array
+        int[] qp; //inverse: qp[i] = the index of the key in the priority queue, qp[i] is set to -1 initially to incate the i-th Key is not in the priority queue 
 
         public IndexMinPQ(int maxSize)
         {
             this.maxSize = maxSize;
-            pq = new T[maxSize + 1];
+            pq = new int[maxSize + 1];
+            qp = new int[maxSize + 1];
+            keys = new T[maxSize + 1];
             size = 0;
 
+        }
+
+        public bool IsEmpty()
+        {
+            return size==0;
+        }
+
+        public bool Contains(int i)
+        {
+            return qp[i] != -1;
+        }
+
+        public void Insert(int i , T key)
+        {
+            size++;
+            qp[i] = size;
+            keys[i] = key;
+            pq[size] = i;
+            Swim(size);
+        }
+
+        public void Changekey(int i, T key)
+        {
+            keys[i] = key;
+            Swim(qp[i]);//question: how do we do Swim and Sink together?
+            Sink(qp[i]);
+        }
+
+        public T MinKey()
+        {
+            return keys[pq[1]];
+        }
+
+        public int MinIndex()
+        {
+            return pq[1];
+        }
+
+        //return the index of min key in the keys[] array
+        //Question: why return the indx but not the min value?
+        public int DelMin()
+        {
+            if (IsEmpty())
+                throw new Exception("no min in the empty queue");
+
+            int indexOfMin = pq[1];
+            if (size > 1)
+            {
+                exch(1, size);
+                size--;
+                Sink(1);
+            }
+            else
+                size--;
+            keys[pq[size + 1]] = default(T);//set to null
+            qp[pq[size + 1]] = -1;
+
+            return indexOfMin;
+            
+        }
+
+        public void Delete(int i)
+        {
+            int index = qp[i];
+            exch(index, size);
+            size--;
+            Swim(index);//question : how do we do Swim and Sink together?
+            Sink(index);
+            keys[index] = default(T);
+            qp[i] = -1;
+        }
+
+        //bottom-up reheapify
+        void Swim(int i)
+        {
+            while ((i > 1) && Great(i / 2, i))
+            {
+                exch(i, i / 2); //exchang, the current with the parent;
+                i = i / 2;  //get the parent;
+            }
+        }
+
+        //top-down reheapify
+        void Sink(int i)
+        {
+            while (i * 2 <= size)//Only process if the current(i) has child
+            {
+
+                int j = i * 2; //j is left child now
+                if ((j < size) && Less(j + 1, j)) //if right child is smaller than left child, set j to right child
+                    j = j + 1;
+                if (Great(i, j)) //if the current is greater than the child 
+                {
+                    exch(i, j);
+                    i = j;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        bool Great(int i, int j)
+        {
+            return pq[i].CompareTo(pq[j]) > 0;
+        }
+        bool Less(int i, int j)
+        {
+            return pq[i].CompareTo(pq[j]) < 0;
+        }
+
+        void exch(int i, int j)
+        {
+            //T temp = pq[i];
+            //pq[i] = pq[j];
+            //pq[j] = temp;
         }
     }
 }
