bd50400340e26c495f4ecd1adbcab3bd65483e4c
Wei Xia <wei_xia@hotmail.com>
2017-05-17 21:21:14 -0400

    Completed the Dijkstra class

--- ALGRKC/ALGRKC/Source/SPT/DijkstraSP.cs
+++ ALGRKC/ALGRKC/Source/SPT/DijkstraSP.cs
@@ -14,7 +14,45 @@ namespace ALGRKC.Source.SPT
         DirectedEdge[] edgeTo;
         IndexMinPQ<double> pq;
 
-        public DijkstraSP()
+        public DijkstraSP(EdgeWeightedDiagraph g, int source)
+        {
+            vertexNumber = g.V();
+            distTo = new double[vertexNumber];
+            for (int i = 0; i < vertexNumber; i++)
+                distTo[i] = double.MinValue;
+            distTo[source] = 0.0;
+
+            edgeTo = new DirectedEdge[vertexNumber];
+
+            pq = new IndexMinPQ<double>(vertexNumber);
+            pq.Insert(source, 0.0);
+
+            while(!pq.IsEmpty())
+            {
+                int current = pq.DelMin();
+                Relax(g, current);
+            }
+        }
+
+        void Relax(EdgeWeightedDiagraph g, int v)
+        {
+            foreach(DirectedEdge e in g.AdjList(v))
+            {
+                int w = e.To();
+                double weight = e.Weight();
+                if (distTo[w] > distTo[v] + weight)
+                {
+                    //relax
+                    edgeTo[w] = e;
+                    distTo[w] = distTo[v] + weight;
+                    if (pq.Contains(w))
+                        pq.ChangeKey(w, distTo[w]);
+                    else
+                        pq.Insert(w, distTo[w]);
+                }
+
+            }
+        }
 
 
     }
