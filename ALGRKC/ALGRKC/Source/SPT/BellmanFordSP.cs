0611cd6558438ea411617a14ea508fc795de9c56
Wei Xia <wei_xia@hotmail.com>
2017-06-15 21:14:51 -0700

    added counting sort and BellmenFord

---
+++ ALGRKC/ALGRKC/Source/SPT/BellmanFordSP.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGRKC.Source.SPT
+{
+    //(bellman-Ford algorithm) The following method solves the singlesource
+    //shortest-paths problem from a given source s for any edge-weighted digraph
+    //with V vertices and no negative cycles reachable from s: Initialize distTo[s]
+    //to 0 and all other distTo[] values to infinity.Then, considering the digraph’s edges
+    //in any order, relax all edges. Make V-1 such passes.
+    public class BellmanFordSP
+    {
+        int vertexNum;
+        double[] distTo;
+        DirectedEdge[] edgeTo;
+
+        //Bellman Ford has the complexity of O(VE)
+        public BellmanFordSP(EdgeWeightedDiagraph ewg, int source)
+        {
+            vertexNum = ewg.V();
+            distTo = new double[vertexNum];
+            edgeTo = new DirectedEdge[vertexNum];
+
+            for (int i = 0; i < vertexNum; i++)
+                distTo[i] = double.PositiveInfinity;
+            distTo[source] = 0.0;
+
+
+        }
+
+        void Relax(EdgeWeightedDiagraph ewg, int v)
+        {
+
+        }
+    }
+}
---
+++ ALGRKC/ALGRKC/Source/Sort/MiscSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGRKC.Source.Sort
+{
+    class MiscSort
+    {
+        //sort assumes that each of the n input elements is an integer in the range
+        //0 to k, for some integer k. the complexity is O(n+k)
+        static void CountingSort(int[] org, int[] result, int k)
+        {
+            int[] counts = new int[k+1]; //create the counting array
+            for (int i = 0; i <= k; i++)
+                counts[i] = 0; //initialize the original counts to 0 for each possible values 0 to k
+
+            
+            int size = org.Length;
+
+            for (int i = 0; i < size; i++)
+                result[i] = 0;
+
+            for (int i = 0; i < size; i++)
+                counts[org[i]]++;
+
+            for (int i = 1; i <= k; i++)
+                counts[i] = counts[i] + counts[i - 1];//accumulate the counts so that counts[i] now denotes index+1 of the last i in the sorted array
+
+            for(int i=size-1;i>=0;i--)
+            {
+                result[counts[org[i]] - 1] = org[i];
+                counts[org[i]]--;
+            }
+        }
+    }
+}
