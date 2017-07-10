db24e9187798f990c80e1c158aae301b4312dc47
Wei Xia <wei_xia@hotmail.com>
2017-07-09 19:58:42 -0700

    return Point from SaddleSearch method

--- ALGRKC/ALGRKC/Source/Sort/SortMisc.cs
+++ ALGRKC/ALGRKC/Source/Sort/SortMisc.cs
@@ -6,6 +6,27 @@ using System.Threading.Tasks;
 
 namespace ALGRKC.Source.Sort
 {
+    struct Point
+    {
+        int x, y;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X
+        {
+            get { return x; }
+            set { this.x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { this.y = value; }
+        }
+    }
     class SortMisc
     {
 
@@ -17,7 +38,7 @@ namespace ALGRKC.Source.Sort
         //  2   5   8   10  17
         //  5   8   8   12  20
         //  10  12  20  25  27
-        public static void SaddleSearch(int[][] arr, int target)
+        public static Point SaddleSearch(int[][] arr, int target)
         {
             //the strategy is starting fromt the top-right corner, if the element > target, go left(col-1);
             //if element>target, go below(row+1); if element==target, find match;
@@ -25,20 +46,28 @@ namespace ALGRKC.Source.Sort
             int rowNumnber = arr.GetLength(0);
             int colNumber = arr.GetLength(1);
 
+            Point p = new Point(1, -1);
+
             int currRow = 0, currCol = colNumber - 1;
 
             while(currCol>0 & currRow<rowNumnber)
             {
                 if (arr[currRow][currCol] == target)
                 {
-                    Console.WriteLine("found match at row " + currRow + ", " + currCol);
-                    return;
+                    //Console.WriteLine("found match at row " + currRow + ", " + currCol);
+                    p.X = currRow;
+                    p.Y = currCol;
+
+                    return p;
+
                 }
                 else if (arr[currRow][currCol] > target)
                     currCol--;
                 else
                     currRow++;
             }
+
+            return p;
 
         }
     }
