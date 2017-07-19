23167b7a6f68425c3a989c84eedba2c84244c59f
Wei Xia <wei_xia@hotmail.com>
2017-07-19 16:23:04 -0700

    aded comments to SubarrayClosettoZero

--- ALGRKC/ALGRKC/Source/Misc/Goog.cs
+++ ALGRKC/ALGRKC/Source/Misc/Goog.cs
@@ -75,25 +75,28 @@ namespace ALGRKC.Source.Misc
         //solution: The idea is to (1) calculate the sum of the prefix array
         //(2) order the sums 
         //(3) pick up the smallest difference of the two adjacent sums
-        //Complexity is O(N)
+        //Complexity is O(NlgN), as it's determined by the sorting
+        //Note: if all the elements are integer, we may use Hashtable to store the preFix sum array.
+        // and check if the value is already in the hashtable/hasmap everytime we get a(next) prefix sum.
+        // so the complexity can be O(N)
         public static int SubArrayClosestToZero(int[] arr)
         {
             int[] prefixSum = new int[arr.Length];
 
-            //calculate the prefix sum
+            //calculate the prefix sum , O(N)
             prefixSum[0] = arr[0];
             for(int i =1;i<arr.Length;i++)
             {
                 prefixSum[i] = prefixSum[i - 1] + arr[i];
             }
 
-            //sort the sums
+            //sort the sums(O(NlgN))
             QuickSort qc = new QuickSort();
             qc.QC(prefixSum, 0, prefixSum.Length - 1);
 
             int min = Int32.MaxValue;
             int currDiff = 0;
-            //find the min difference
+            //find the min difference -- O(N)
             for(int i=1;i<prefixSum.Length;i++)
             {
                 currDiff = (prefixSum[i] - prefixSum[i - 1]);
@@ -104,5 +107,46 @@ namespace ALGRKC.Source.Misc
             return min;
 
         }
+
+        //Find a sub-array whose sum is equal to K.
+        //the array only contains integers
+        //Stragegy: use prefix array and hashtable
+        public static Tuple<int, int>  SubArraySum(int[] arr, int sum)
+        {
+            Tuple<int, int> pair;// = new Tuple<int, int>();
+            int[] prefixSum = new int[arr.Length];
+
+            //calculate the prefix sum , O(N)
+            prefixSum[0] = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                prefixSum[i] = prefixSum[i - 1] + arr[i];
+            }
+
+            HashSet<int> ht = new HashSet<int>();
+            for(int i =0;i<arr.Length;i++)
+            {
+                if (prefixSum[i] == sum)
+                {
+                    pair = new Tuple<int, int>(0, i);
+                    return pair;
+                }
+                else if (ht.Contains(sum - prefixSum[i]) || ht.Contains(prefixSum[i] - sum))
+                {
+                    pair = new Tuple<int, int>(0, i);// needs to recode here
+                    return pair;
+                }
+                else
+                    ht.Add(prefixSum[i]);
+                
+                
+            }
+
+            return new Tuple<int, int>(-1, -1);//no found
+
+
+
+        }
     }
+
 }
