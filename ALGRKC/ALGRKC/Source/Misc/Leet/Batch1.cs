cb66b376af2e9f7f826da83c49bd1e3032028577
Wei Xia <wei_xia@hotmail.com>
2017-10-19 22:07:37 -0700

    added omments to LengthOfLongestSubstring

--- ALGRKC/ALGRKC/Source/Misc/Leet/Batch1.cs
+++ ALGRKC/ALGRKC/Source/Misc/Leet/Batch1.cs
@@ -21,16 +21,18 @@ namespace ALGRKC.Source.Misc.Leet
 
             int start = 0, end = -1, maxStart = 0, maxEnd = 0, tempStart=0 ;
             int currentMax = 1;
-            int[] visited = new int[26]; //we assume only the lower case character a-z is allowed
-            for (int i = 0; i < 26; i++)
-                visited[i] = -1;
+            int[] visited = new int[26]; //we assume only the lower case character a-z is allowed, this is the hashtable, we can also use hashset here
+            for (int i = 0; i < 26; i++) 
+                visited[i] = -1; //initialize to -1 , which means the character is not encounterd in the current slide window
 
             int index = -1;
 
             for(int i=0;i<length;i++)
             {
                 index = s[i] - 97;
-                if (visited[index] == -1) //in the current slide window, the character being visisted has never been hit before
+                if (visited[index] == -1) //in the current slide window, the character being visisted has never been hit before. 
+                                         //We can potentially allow the character the current character to be counted even if we hit it before 
+                                         //as long as the last hit is before the current start. Later we just need to update the visited[index] to be the index of the last one visited. 
                 {
                     visited[index] = i;
                     end++;
@@ -38,7 +40,7 @@ namespace ALGRKC.Source.Misc.Leet
                 }
                 else// the ith elment has appeared before in the current range
                 {
-                    if(end -start+1 > currentMax) //caculate the max and update max, start, end if needed.
+                    if(end -start+1 > currentMax) //caculate the max and update max, start, end if needed. 
                     {
                         currentMax = end - start + 1;
                         maxStart = start;
@@ -50,7 +52,7 @@ namespace ALGRKC.Source.Misc.Leet
                     
                     tempStart = visited[index] + 1;
 
-                    //1. demark from start to visted index
+                    //1. reset the visisted to -1 from start to visted index. 
                     for (int j = start; j<= visited[index]; j++)
                         visited[s[j]-97] = -1;
 
@@ -65,7 +67,7 @@ namespace ALGRKC.Source.Misc.Leet
 
             }
 
-            if (end - start + 1 > currentMax) //caculate the max and update max, start, end if needed.
+            if (end - start + 1 > currentMax) //caculate the max and update max, start, end if needed as this is the last window
             {
                 currentMax = end - start + 1;
                 maxStart = start;
