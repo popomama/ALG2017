6782424a76e1c6df43eaae388a5c5a5945daf725
Wei Xia <wei_xia@hotmail.com>
2017-10-19 20:36:15 -0700

    added LengthOfLongestSubstring

---
+++ ALGRKC/ALGRKC/Source/Misc/Leet/Batch1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGRKC.Source.Misc.Leet
+{
+    public class Batch1
+    {
+
+        //Leetcode #3
+        //Given a string, find the length of the longest substring without repeating characters.
+        //Examples:
+        //Given "abcabcbb", the answer is "abc", which the length is 3.
+        //Given "bbbbb", the answer is "b", with the length of 1.
+        //Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int length = s.Length;
+
+            int start = 0, end = -1, maxStart = 0, maxEnd = 0, tempStart=0 ;
+            int currentMax = 1;
+            int[] visited = new int[26]; //we assume only the lower case character a-z is allowed
+            for (int i = 0; i < 26; i++)
+                visited[i] = -1;
+
+            int index = -1;
+
+            for(int i=0;i<length;i++)
+            {
+                index = s[i] - 97;
+                if (visited[index] == -1) //in the current slide window, the character being visisted has never been hit before
+                {
+                    visited[index] = i;
+                    end++;
+
+                }
+                else// the ith elment has appeared before in the current range
+                {
+                    if(end -start+1 > currentMax) //caculate the max and update max, start, end if needed.
+                    {
+                        currentMax = end - start + 1;
+                        maxStart = start;
+                        maxEnd = end;
+   
+                    }
+                    //now we need to do the clean up, 
+                    
+                    
+                    tempStart = visited[index] + 1;
+
+                    //1. demark from start to visted index
+                    for (int j = start; j<= visited[index]; j++)
+                        visited[s[j]-97] = -1;
+
+                    //2. reset start and i
+                    start = tempStart;
+                    end = i;
+
+                    //3. update visited[index] to i;
+                    visited[index] = i;
+
+                }
+
+            }
+
+            if (end - start + 1 > currentMax) //caculate the max and update max, start, end if needed.
+            {
+                currentMax = end - start + 1;
+                maxStart = start;
+                maxEnd = end;
+
+            }
+
+            return currentMax;
+        }
+    }
+}
---
+++ ALGRKC/TestALG/Source/Misc/Leet/Batch1Tests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ALGRKC.Source.Misc.Leet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGRKC.Source.Misc.Leet.Tests
+{
+    [TestClass()]
+    public class Batch1Tests
+    {
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest()
+        {
+            string s ="abcddcba";
+            int len;
+            len=Batch1.LengthOfLongestSubstring(s);
+            Assert.AreEqual(4, len);
+
+            s = "abcdkmcbha";
+            len = Batch1.LengthOfLongestSubstring(s);
+            Assert.AreEqual(7, len);
+
+        }
+    }
+}
