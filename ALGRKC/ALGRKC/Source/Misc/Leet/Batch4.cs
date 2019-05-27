524c9c600c37c3ef3d812828f34be14694970eba
Wei Xia <wei_xia@hotmail.com>
2019-05-26 17:31:05 -0700

    added Leetcode 153: Find Minimum in Rotated Sorted Array

--- ALGRKC/ALGRKC/Source/Misc/Leet/Batch4.cs
+++ ALGRKC/ALGRKC/Source/Misc/Leet/Batch4.cs
@@ -363,5 +363,49 @@ namespace ALGRKC.Source.Misc.Leet
             
 
         }
+
+        //Leetcode 153: Find Minimum in Rotated Sorted Array
+        //Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+        //        (i.e.,  [0, 1, 2, 4, 5, 6, 7] might become[4, 5, 6, 7, 0, 1, 2]).
+
+        //Find the minimum element.
+
+        //You may assume no duplicate exists in the array.
+
+        //Example 1:
+
+        //Input: [3,4,5,1,2]
+        //Output: 1
+
+        //Example 2:
+
+        //Input: [4,5,6,7,0,1,2]
+        //Output: 0
+        public int FindMin(int[] nums)
+        {
+            //int min = FindMinBinary(nums, 0, nums.Length - 1);
+            //return min;
+
+            int left = 0, right = nums.Length - 1;
+            int mid;
+
+
+            while (left < right - 1)
+            {
+                mid = (left + right) / 2;
+                if (nums[mid] > nums[right])
+                    left = mid + 1;
+                else
+                    right = mid;
+            }
+
+            if (left == right)
+                return nums[left];
+            else
+                return Math.Min(nums[left], nums[right]);
+        }
+
+       
     }
 }
